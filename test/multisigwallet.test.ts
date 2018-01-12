77e2bf0f4bff50cabcf44401c02df87cc0827564
harzo <harzojunior@gmail.com>
2018-01-12 13:42:28 +0100

    Remove redundant context class
    Reorganize MultiSigWallet tests to work without context class.

--- test/multisigwallet.test.ts
+++ test/multisigwallet.test.ts
@@ -1,12 +1,10 @@
 import { assert } from 'chai';
-import { without } from 'ramda';
 
 import { ContractContextDefinition } from 'truffle';
 import * as Web3 from 'web3';
 
 import { MultiSigWallet, OwnerAdditionEvent, WalletArtifacts } from 'wallet';
 
-import { MultiSigWalletTestContext } from './context';
 import { assertReverts, findLastLog } from './helpers';
 
 declare const web3: Web3;
@@ -19,13 +17,13 @@ contract('MultiSigWallet', accounts => {
   const owners = [accounts[0], accounts[1], accounts[2]];
   const required = 2;
 
-  async function createWallet(options: any = {}) {
-    return await MultiSigWalletContract.new(
-      options.owners || owners,
-      options.required || required,
-      { from: options.from || owners[0] }
-    );
-  }
+  let wallet: MultiSigWallet;
+
+  beforeEach(async () => {
+    wallet = await MultiSigWalletContract.new(owners, required, {
+      from: owners[0]
+    });
+  });
 
   describe('#ctor', () => {
     it('should revert for empty list of owners', async () => {
@@ -41,59 +39,42 @@ contract('MultiSigWallet', accounts => {
     });
 
     it('should create wallet', async () => {
-      const wallet = await createWallet();
       assert.ok(wallet);
     });
 
     it('should set required', async () => {
-      const wallet = await createWallet();
       assert.equal(await wallet.required(), required);
     });
 
     it('should set owners', async () => {
-      const wallet = await createWallet();
       assert.deepEqual(await wallet.getOwners(), owners);
     });
   });
 
-  const ctx = new MultiSigWalletTestContext<MultiSigWallet>(
-    without(owners, accounts),
-    owners,
-    required
-  );
-
-  beforeEach(async () => {
-    ctx.wallet = await createWallet({ from: ctx.owners[0] });
-  });
-
   describe('#addOwner', () => {
-    testAddOwner(ctx);
+    const newOwner = accounts[0];
+
+    it('should emit OwnerAdditionEvent', async () => {
+      const tx = await wallet.addOwner(newOwner, {
+        from: owners[0]
+      });
+
+      const log = findLastLog(tx, 'OwnerAddition');
+      assert.isOk(log);
+
+      const event = log.args as OwnerAdditionEvent;
+      assert.isOk(event);
+    });
+
+    it('should add new owner', async () => {
+      await wallet.addOwner(newOwner, {
+        from: owners[0]
+      });
+
+      const walletOwners = await wallet.getOwners();
+
+      assert.equal(owners.length, 4);
+      assert.equal(owners[3], newOwner);
+    });
   });
 });
-
-export function testAddOwner(ctx: MultiSigWalletTestContext<MultiSigWallet>) {
-  const newOwner = ctx.accounts[0];
-
-  it('should emit OwnerAdditionEvent', async () => {
-    const tx = await ctx.wallet.addOwner(newOwner, {
-      from: ctx.owners[0]
-    });
-
-    const log = findLastLog(tx, 'OwnerAddition');
-    assert.isOk(log);
-
-    const event = log.args as OwnerAdditionEvent;
-    assert.isOk(event);
-  });
-
-  it('should add new owner', async () => {
-    await ctx.wallet.addOwner(newOwner, {
-      from: ctx.owners[0]
-    });
-
-    const owners = await ctx.wallet.getOwners();
-
-    assert.equal(owners.length, 4);
-    assert.equal(owners[3], newOwner);
-  });
-}
