7713025065804b7a9698af09b02423827e5ec657
Jakub Stefanski <code@jakubstefanski.com>
2018-01-16 11:17:50 +0100

    Increase gas price

--- truffle-config.ts
+++ truffle-config.ts
@@ -4,14 +4,14 @@ export = {
     networks: {
         mainnet: {
             from: '0x6c14a683e3e894fbEE5F929C5fAC402dF79694f6',
-            gasPrice: fromGwei(21),
+            gasPrice: fromGwei(51),
             host: 'localhost',
             network_id: '1',
             port: 8545
         },
         rinkeby: {
             from: '0x6c14a683e3e894fbEE5F929C5fAC402dF79694f6',
-            gasPrice: fromGwei(20),
+            gasPrice: fromGwei(51),
             host: 'localhost',
             network_id: '4',
             port: 8545
