7d8e93b6da18454e433396b753135f35eb187ffb
Jakub Stefanski <code@jakubstefanski.com>
2018-01-16 09:06:27 +0100

    Add networks configuration

--- truffle-config.ts
+++ truffle-config.ts
@@ -1,4 +1,26 @@
+import { fromGwei } from './utils';
+
 export = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+    networks: {
+        mainnet: {
+            from: '0x6c14a683e3e894fbEE5F929C5fAC402dF79694f6',
+            gasPrice: fromGwei(21),
+            host: 'localhost',
+            network_id: '1',
+            port: 8545
+        },
+        rinkeby: {
+            from: '0x6c14a683e3e894fbEE5F929C5fAC402dF79694f6',
+            gasPrice: fromGwei(20),
+            host: 'localhost',
+            network_id: '4',
+            port: 8545
+        },
+        testrpc: {
+            gasPrice: 0,
+            host: 'localhost',
+            network_id: '*',
+            port: 8545
+        }
+    }
 };
