90da0b9edef5c9b78a08295c211ee18033a7f3db
Jakub Stefanski <code@jakubstefanski.com>
2018-01-16 11:55:38 +0100

    Refactor truffle-config

--- truffle-config.ts
+++ truffle-config.ts
@@ -1,26 +1,35 @@
 import { fromGwei } from './utils';
 
+const gas = 3000000;
+const gasPrice = fromGwei(51);
+const host = 'localhost';
+const port = 8545;
+
+const defaults = {
+  gas,
+  gasPrice,
+  host,
+  port
+};
+
 export = {
-    networks: {
-        mainnet: {
-            from: '0x6c14a683e3e894fbEE5F929C5fAC402dF79694f6',
-            gasPrice: fromGwei(51),
-            host: 'localhost',
-            network_id: '1',
-            port: 8545
-        },
-        rinkeby: {
-            from: '0x6c14a683e3e894fbEE5F929C5fAC402dF79694f6',
-            gasPrice: fromGwei(51),
-            host: 'localhost',
-            network_id: '4',
-            port: 8545
-        },
-        testrpc: {
-            gasPrice: 0,
-            host: 'localhost',
-            network_id: '*',
-            port: 8545
-        }
+  networks: {
+    mainnet: {
+      ...defaults,
+      from: '0x6c14a683e3e894fbEE5F929C5fAC402dF79694f6',
+      network_id: '1'
+    },
+    rinkeby: {
+      ...defaults,
+      from: '0x6c14a683e3e894fbEE5F929C5fAC402dF79694f6',
+      network_id: '4'
+    },
+    testrpc: {
+      gas,
+      gasPrice: 0,
+      host,
+      network_id: '*',
+      port
     }
+  }
 };
