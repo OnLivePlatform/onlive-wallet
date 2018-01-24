286ae84fde1cec2e06d4585773147c76c27d6a2c
harzo <harzojunior@gmail.com>
2018-01-24 11:07:48 +0100

    Revert migration owners to origin

--- migrations/2_deploy_multisig.ts
+++ migrations/2_deploy_multisig.ts
@@ -1,13 +1,17 @@
 import { Deployer, TruffleArtifacts } from 'truffle';
-import * as Web3 from 'web3';
 
 declare const artifacts: TruffleArtifacts;
-declare const web3: Web3;
 
 const MultiSigWallet = artifacts.require('./MultiSigWallet.sol');
 
 async function deploy(deployer: Deployer) {
-  const owners = web3.eth.accounts.slice(0, 5);
+  const owners = [
+    '0x594F5804Eb71d6aB16753D7247D7DD031245fBE7',
+    '0x886351b3135Ea6523BA495f27EdE6982D6936E41',
+    '0xb2514c463784333aDA49Aca9394515F04bB01471',
+    '0x529E8Fc0AE96409b89f7255eFe9eBE3A55780F2f',
+    '0x6633b7233B63521b4697bBe92100Cf4A1BF70829'
+  ];
   const required = 3;
 
   await deployer.deploy(MultiSigWallet, owners, required);
